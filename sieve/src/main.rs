67b15416df7685596ec286f0b045cc69adc0dc2e
Luigi Foscari <luigi.foscari@icloud.com>
2022-08-18 12:36:24 +0200

    fixed file management issue

--- sieve/src/main.rs
+++ sieve/src/main.rs
@@ -21,14 +21,14 @@ fn flush_sieve(sieve: &mut Vec<u64>, auxiliary: &mut fs::File) -> Result<(), std
     sorting_helper.sort_by(|&i, &j| sieve[i].cmp(&sieve[j]));
 
     // 2
-    let mut marked_urls = vec![false; sieve.len()];
+    let mut unique_urls = vec![false; sieve.len()];
     let mut duplicate_canary = 0;
 
     for sorted_index in &sorting_helper {
         let url_sign = sieve[*sorted_index];
 
         if url_sign != duplicate_canary {
-            marked_urls[*sorted_index] = true;
+            unique_urls[*sorted_index] = true;
         }
 
         duplicate_canary = url_sign;
@@ -36,7 +36,7 @@ fn flush_sieve(sieve: &mut Vec<u64>, auxiliary: &mut fs::File) -> Result<(), std
 
     // 3
     let known_urls: Vec<u64> = fs::read_to_string("known.urls")
-        .expect("Cannot find know urls file")
+        .unwrap_or(String::from(""))
         .lines()
         .map(|s| u64::from_str_radix(s, 10).unwrap())
         .collect();
@@ -51,7 +51,7 @@ fn flush_sieve(sieve: &mut Vec<u64>, auxiliary: &mut fs::File) -> Result<(), std
     let mut sieve_index: usize = 0;
     let mut known_index: usize = 0;
 
-    let mut discovered_urls = vec![];
+    let mut discovered_urls = vec![false; sieve.len()];
 
     loop {
         if sieve_index >= sieve.len() {
@@ -68,25 +68,26 @@ fn flush_sieve(sieve: &mut Vec<u64>, auxiliary: &mut fs::File) -> Result<(), std
             break;
         }
 
-        let sieve_url = sieve[sorting_helper[sieve_index]];
-        let known_url = known_urls[known_index];
-
-        if !marked_urls[sorting_helper[sieve_index]] {
+        if !unique_urls[sorting_helper[sieve_index]] {
             sieve_index += 1;
             continue;
         }
 
-        match sieve_url.cmp(&known_url) {
+        let sieve_url_sign = sieve[sorting_helper[sieve_index]];
+        let known_url_sign = known_urls[known_index];
+
+        match sieve_url_sign.cmp(&known_url_sign) {
             Ordering::Greater => {
-                let _ = write!(new_known_urls, "{:?}\n", known_url);
+                write!(new_known_urls, "{:?}\n", known_url_sign)?;
                 known_index += 1;
             }
             Ordering::Less => {
-                let _ = write!(new_known_urls, "{:?}\n", sieve_url);
+                write!(new_known_urls, "{:?}\n", sieve_url_sign)?;
+                discovered_urls[sorting_helper[sieve_index]] = true;
                 sieve_index += 1;
             }
             Ordering::Equal => {
-                let _ = write!(new_known_urls, "{:?}\n", sieve_url);
+                write!(new_known_urls, "{:?}\n", sieve_url_sign)?;
                 known_index += 1;
                 sieve_index += 1;
             }
@@ -102,7 +103,11 @@ fn flush_sieve(sieve: &mut Vec<u64>, auxiliary: &mut fs::File) -> Result<(), std
     let reader = BufReader::new(auxiliary);
     
     for (index, url) in reader.lines().enumerate() {
-        if marked_urls[index] {
+        if index >= sieve.len() {
+            break;
+        }
+
+        if discovered_urls[index] {
             println!("{:?}", url);
         }
     }
@@ -136,11 +141,18 @@ fn main() -> Result<(), std::io::Error> {
             auxiliary.flush()?;
             flush_sieve(&mut sieve, &mut auxiliary)?;
 
+            auxiliary.rewind()?;
+            auxiliary.set_len(0)?;
+
             while !sieve.is_empty() {
                 sieve.pop();
             }
         }
+
     }
+    
+    fs::remove_file("auxiliary.urls")?;
+    fs::remove_file("known.urls")?;
 
     Ok(())
 }
